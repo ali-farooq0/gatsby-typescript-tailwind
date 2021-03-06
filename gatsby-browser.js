b193245c90c46ba8722d5d208ec7bca57f39ccfa
Ali Farooq <ali.farooq0@pm.me>
2021-03-06 13:17:48 -0500

    Working chakra-ui

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,4 +3,17 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-import "./src/css/index.css"
+import './src/css/index.css';
+import React from 'react';
+import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/react';
+
+const customTheme = extendTheme({});
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ChakraProvider theme={customTheme}>
+      <CSSReset />
+      {element}
+    </ChakraProvider>
+  );
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,21 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [isResettingCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        isResettingCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
